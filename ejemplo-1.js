7c4291beac39c7541f64adfb2bab5370c2a1a2d4
jose Brazon <abg.brazon@gmail.com>
2022-11-10 20:31:37 -0300

    esto es java

---
+++ ejemplo-1.js
@@ -0,0 +1,33 @@
+//principales palabras reservadas
+
+//son palabras propias del lenguaje a utilizar
+
+//let ; var ; const;--Estas son palabras para definir variables
+
+// datos tipo string=una cadena de caracteres
+
+let nombre = "Jose" //esto es una variable string
+
+let edad =2203  //dato tipo number, es una variable
+
+let enCurso = true // esta variable es un boleano
+
+let sentimiento = null // esta variables es un objeto
+
+let aprendizaje
+
+let array =[nombre, edad, enCurso, sentimiento, aprendizaje]  //nos permite guardar distintos datos
+
+console.log(array)
+
+console.log(typeof(aprendizaje))
+
+console.log(typeof(sentimiento))
+
+console.log(typeof(enCurso))
+
+ console.log(nombre);
+console.log(typeof(nombre))
+
+ console.log(edad)
+console.log(typeof(edad))
